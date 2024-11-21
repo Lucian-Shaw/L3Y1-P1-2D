f625049d4050c9bc86c49f696acabd7e447d2f77
Lucian-Shaw <Sha24004780@heartofyorkshire.ac.uk>
2024-11-21 16:12:30 +0000

    enemy snf combvat
    bullets go wee, applied new epic mechanics gamr skills 100

--- 2d platformer R VS M/Assets/Scripts/PlayerController.cs
+++ 2d platformer R VS M/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,6 +11,15 @@ public class PlayerController : MonoBehaviour
     [Header("UI")]
     public TMP_Text timerTxt;
     public float timer;
+
+    [Header("Health")]
+    public int maxHealth;
+    public int currentHealth;
+
+    [Header("Shooting")]
+    public Transform shootingPoint ;
+    public GameObject bullet;
+    bool isFacingRight;
 
     [Header("Main")]
     public float moveSpeed;
@@ -26,6 +37,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         startPos = transform.position;
+
+        currentHealth = maxHealth;
+        isFacingRight = true;
     }
 
     // Update is called once per frame
@@ -35,6 +49,9 @@ public class PlayerController : MonoBehaviour
         timerTxt.text = timer.ToString("F2");
         
         Movement();
+        Health();
+        shoot();
+        MovementDirection();
     }
 
     void Movement()
@@ -54,8 +71,44 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void Health()
+    {
+        if (currentHealth <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+
+     void shoot()
+     {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(bullet, shootingPoint.position, shootingPoint.rotation);
+        }
+     }
+     
+    void MovementDirection()
+    {
+        if (isFacingRight && inputs < -.1f)
+        {
+            Flip();
+        }
+        else if (!isFacingRight && inputs > .1f)
+        {
+            Flip();
+        }
+ 
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
     private void OnTriggerEnter2D(Collider2D other) 
     {
+    
         if (other.gameObject.CompareTag("Hazard"))
         {
             transform.position = startPos;
@@ -63,6 +116,11 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Exit"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            currentHealth--;
+            Destroy(other.gameObject);
         }
     }
 }
